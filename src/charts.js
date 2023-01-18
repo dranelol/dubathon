3fad09dc94ab853e9146958c6a61997524cdee44
MattWallace <cardsfan853@gmail.com>
2023-01-18 14:09:04 -0600

    adding chatter and emoji data

--- src/charts.js
+++ src/charts.js
@@ -1,28 +1,32 @@
-import { Chart } from 'chart.js/auto'
-let data = require('../data/release/dubathon_data.json');
+//import { Chart } from 'chart.js/auto';
+//import 'chartjs-adapter-luxon';
+import zoomPlugin from 'chartjs-plugin-zoom';
 
+//import DownsamplePlugin from 'chartjs-plugin-downsample';
+let data = require('../data/release/dubathon_data.json');
+let maiyaData = require('../data/release/maiyaDanceOverTime.json');
+let chatData = require('../data/release/chatters.json');
+let emoteData = require('../data/release/emojiUse.json');
+
+window.activateChart = function(chartIdx) {
+  activateChart(chartIdx);
+};
+
+Chart.register(zoomPlugin);
 
 Chart.defaults.font.family = "futura";
 
-const dataOne = {
-  labels: [
-    'subs',
-    'bitties',
-    'donos'
-  ],
-  datasets: [{
-    label: 'dollars by type',
-    data: [300, 50, 100],
-    backgroundColor: [
-      'rgb(255, 99, 132)',
-      'rgb(54, 162, 235)',
-      'rgb(255, 205, 86)'
-    ],
-    hoverOffset: 4
-  }]
-};
-
 var currentChart = null;
+
+// stream started at November 22, 2022, 08:44:42 PM EST (Tuesday), or 20:44:42
+// stream ended at December 01, 2022, 08:02:33 PM EST (Thursday), or 20:02:33
+
+// at stream start, there were 4 subs and 0:47:18 on the clock, or 2838 seconds
+// the first data entry starts at 2022-11-22 19:48:20; rolling back to this, stream timer would have been 56:22 more, 3382, minus the 4 subs (120) so 3262
+// timer starting at first data entry gives 6100 seconds or 1:41:40
+
+const originalTimerMS = 6100000;
+const firstDateEventMS = 1669168100000;
 
 export function activateChart(chartIdx) {
   
@@ -49,8 +53,286 @@ export function activateChart(chartIdx) {
     case 5:
       moneyPerDaySplit();
       break;
+    case 6:
+      timerDurationCountdown();
+      break;
+    case 7:
+      maiyaDanceUsage();
+      break;
+    case 8:
+      topChatters();
+      break;
+    case 9:
+      topEmotes();
+      break;
   }
 }
+
+export async function topEmotes() {
+
+  let emotesNames = [];
+  let emotesCount = [];
+  
+  for(let i = 0; i < emoteData.length; i++) {
+    const dataLine = emoteData[i];
+    emotesCount.push(dataLine.count);
+    emotesNames.push(dataLine.emote);
+  }
+
+  currentChart = new Chart(
+    document.getElementById('charts'),
+    {
+      type: 'bar',
+      data: {
+        labels: emotesNames,
+        datasets: [
+          {
+            label: 'Top Emotes',
+            data: emotesCount
+          }
+        ]
+      },
+      options: {
+        maintainAspectRatio: false,
+        plugins: {
+          zoom: {
+            limits: {
+              y: {min: 0, max: 20000},
+            },
+            pan: {
+              enabled: true,
+              mode: 'xy'
+            },
+            zoom: {
+              wheel: {
+                enabled: true,
+              },
+              pinch: {
+                enabled: true
+              },
+              mode: 'xy',
+            }
+          }
+        }
+      }
+    }
+  );
+};
+
+export async function topChatters() {
+
+  let chatterData = [];
+  let chatterNames = [];
+  
+  for(let i = 0; i < chatData.length; i++) {
+    const dataLine = chatData[i];
+    chatterNames.push(dataLine.chatter);
+    chatterData.push(dataLine.count);
+  }
+
+  currentChart = new Chart(
+    document.getElementById('charts'),
+    {
+      type: 'bar',
+      data: {
+        labels: chatterNames,
+        datasets: [
+          {
+            label: 'Top Chatters',
+            data: chatterData
+          }
+        ]
+      },
+      options: {
+        maintainAspectRatio: false,
+        plugins: {
+          zoom: {
+            limits: {
+              y: {min: 0, max: 140000},
+            },
+            pan: {
+              enabled: true,
+              mode: 'xy'
+            },
+            zoom: {
+              wheel: {
+                enabled: true,
+              },
+              pinch: {
+                enabled: true
+              },
+              mode: 'y',
+            }
+          }
+        }
+      }
+    }
+  );
+};
+
+export async function maiyaDanceUsage() {
+
+  let chartData = [];
+  let tempDateCollection = [];
+
+  let maiyaCount = 0;
+  
+  for(let i = 0; i < maiyaData.length; i++) {
+    const dataLine = maiyaData[i];
+    const emojiCount = dataLine.count;
+    if(emojiCount == "0") {
+      continue;
+    }
+
+    const dateMS = dataLine.timeMS;
+
+    maiyaCount += emojiCount;
+
+    tempDateCollection.push(dateMS);
+    chartData.push(maiyaCount);
+  }
+
+  currentChart = new Chart(
+    document.getElementById('charts'),
+    {
+      type: 'line',
+      data: {
+        labels: tempDateCollection,
+        datasets: [
+          {
+            label: 'MaiyaDance over Time',
+            data: chartData.map((item) => {
+              return item;
+            })
+          }
+        ]
+      },
+      options: {
+        maintainAspectRatio: false,
+        plugins: {
+          zoom: {
+            limits: {
+              x: {min: 1669190944147, max: 1669942956583},
+              y: {min: 0, max: 20000},
+            },
+            pan: {
+              enabled: true,
+              mode: 'xy'
+            },
+            zoom: {
+              wheel: {
+                enabled: true,
+              },
+              pinch: {
+                enabled: true
+              },
+              mode: 'xy',
+            }
+          },
+        },
+        scales: {
+          x: {
+            type: 'time',
+            time: {
+              // Luxon format string
+              tooltipFormat: 'DD T'
+            },
+            title: {
+              display: true,
+              text: 'Date'
+            }
+          },
+        }
+      }
+    }
+  );
+  
+};
+
+export async function timerDurationCountdown() {
+
+  let chartData = [];
+  let tempDateCollection = [];
+
+  let totalTimeAddedMS = 0;
+  
+  for(let i = 0; i < data.length; i++) {
+    const dataLine = data[i];
+    const timeAdded = dataLine.timeadded;
+    if(timeAdded == "0") {
+      continue;
+    }
+
+    const timeAddedNumMS = Number(timeAdded) * 1000;
+    totalTimeAddedMS += timeAddedNumMS;
+    const date = new Date(dataLine.time);
+    const dateMS = date.getTime();
+    const sinceStartMS = dateMS - firstDateEventMS;
+
+    const timerLeft = originalTimerMS - sinceStartMS + totalTimeAddedMS;
+
+    //console.log(timerLeft);
+
+    tempDateCollection.push(dateMS);
+    chartData.push(timerLeft);
+
+  }
+
+  currentChart = new Chart(
+    document.getElementById('charts'),
+    {
+      type: 'line',
+      data: {
+        labels: tempDateCollection,
+        datasets: [
+          {
+            label: 'Hours remaining',
+            data: chartData.map((item) => {
+              return item / 3600000;
+            })
+          }
+        ]
+      },
+      options: {
+        maintainAspectRatio: false,
+        plugins: {
+          zoom: {
+            limits: {
+              y: {min: 0},
+            },
+            pan: {
+              enabled: true,
+              mode: 'xy'
+            },
+            zoom: {
+              wheel: {
+                enabled: true,
+              },
+              pinch: {
+                enabled: true
+              },
+              mode: 'xy',
+            }
+          },
+        },
+        scales: {
+          x: {
+            type: 'time',
+            time: {
+              // Luxon format string
+              tooltipFormat: 'DD T'
+            },
+            title: {
+              display: true,
+              text: 'Date'
+            }
+          },
+        }
+      }
+    }
+  );
+  
+};
 
 
 export async function subsPerDay() {
@@ -92,7 +374,28 @@ export async function subsPerDay() {
         ]
       },
       options: {
-        maintainAspectRatio: false
+        maintainAspectRatio: false,
+        plugins: {
+          zoom: {
+            limits: {
+              y: {min: 0, max: 5000},
+            },
+            pan: {
+              enabled: true,
+              mode: 'xy'
+            },
+            zoom: {
+              wheel: {
+                enabled: true,
+              },
+              pinch: {
+                enabled: true
+              },
+              mode: 'y',
+            }
+          }
+        },
+        
       }
     }
   );
@@ -171,6 +474,26 @@ export async function subsPerDaySplit() {
           y: {
             stacked: true
           }
+        },
+        plugins: {
+          zoom: {
+            limits: {
+              y: {min: 0, max: 3000},
+            },
+            pan: {
+              enabled: true,
+              mode: 'xy'
+            },
+            zoom: {
+              wheel: {
+                enabled: true,
+              },
+              pinch: {
+                enabled: true
+              },
+              mode: 'y',
+            }
+          }
         }
       }
     }
@@ -217,7 +540,27 @@ export async function bitsPerDay() {
         ]
       },
       options: {
-        maintainAspectRatio: false
+        maintainAspectRatio: false,
+        plugins: {
+          zoom: {
+            limits: {
+              y: {min: 0, max: 140000},
+            },
+            pan: {
+              enabled: true,
+              mode: 'xy'
+            },
+            zoom: {
+              wheel: {
+                enabled: true,
+              },
+              pinch: {
+                enabled: true
+              },
+              mode: 'y',
+            }
+          }
+        }
       }
     }
   );
@@ -296,6 +639,26 @@ export async function bitsPerDaySplit() {
           y: {
             stacked: true
           }
+        },
+        plugins: {
+          zoom: {
+            limits: {
+              y: {min: 0, max: 140000},
+            },
+            pan: {
+              enabled: true,
+              mode: 'xy'
+            },
+            zoom: {
+              wheel: {
+                enabled: true,
+              },
+              pinch: {
+                enabled: true
+              },
+              mode: 'y',
+            }
+          }
         }
       }
     }
@@ -366,6 +729,24 @@ export async function moneyPerDay() {
                 }
                 return label;
               }
+            }
+          },
+          zoom: {
+            limits: {
+              y: {min: 0, max: 3000},
+            },
+            pan: {
+              enabled: true,
+              mode: 'xy'
+            },
+            zoom: {
+              wheel: {
+                enabled: true,
+              },
+              pinch: {
+                enabled: true
+              },
+              mode: 'y',
             }
           }
         }
@@ -468,6 +849,24 @@ export async function moneyPerDaySplit() {
                 }
                 return label;
               }
+            }
+          },
+          zoom: {
+            limits: {
+              y: {min: 0, max: 3000},
+            },
+            pan: {
+              enabled: true,
+              mode: 'xy'
+            },
+            zoom: {
+              wheel: {
+                enabled: true,
+              },
+              pinch: {
+                enabled: true
+              },
+              mode: 'y',
             }
           }
         }
